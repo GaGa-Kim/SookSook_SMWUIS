adcbabe6392c85f01b3e84a5a93c48167888a275
seonmin <ghdtjsals14@gmail.com>
2022-07-20 19:49:46 +0900

    판매/나눔 페이지 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import Login from './routes/Login';
 import Join from './routes/Join';
 import Share from './routes/Share';
 import Qaboard from './routes/Qaboard';
+import Sell from './routes/Sell';
 
 function App() {
   return (
@@ -23,11 +24,12 @@ function App() {
         <Route path="/enterboard" element={<EnterBoard />}></Route>
         <Route path="/openstudy" element={<OpenStudy />}></Route>
         <Route path="/setboard" element={<SetBoard />}></Route>
-        <Route path="/membergrade" element={<MemberGrade />}></Route>
+        <Route path="/membergrade" element={<MemberGrade />}></Route>git
         <Route path="/setboard" element={<SetBoard />}></Route>
         <Route path="/board1" element={<Board1 />}></Route>
         <Route path="/board2" element={<Board2 />}></Route>
         <Route path="/private" element={<Private />}></Route>
+        <Route path="/sell" element={<Sell />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/join" element={<Join />}></Route>
         <Route path="/share" element={<Share />}></Route>
