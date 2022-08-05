2cb41336b55efe943c564b810db0ad6cf3c93abb
seonmin <ghdtjsals14@gmail.com>
2022-08-05 21:21:50 +0900

    게시판 수정

--- frontend/src/routes/Sell.js
+++ frontend/src/routes/Sell.js
@@ -7,44 +7,62 @@ import "antd/dist/antd.css";
 import axios from "axios";
 import Logo from "./components/Logo.js";
 
+const Shareblock = () => {
+    return (
+        <section className="block">
+            <button className="share">판매/나눔 게시판</button>
+            <Cwrite />
+        </section>
+    );
+
+};
+
+const Cwrite = () => {
+    return (
+        <section>
+            <button className="newstudy" style={{ marginLeft: "0px" }}>
+                <Link to="/setboard_sell">글 작성하기</Link></button>
+        </section>
+    );
+};
+
 const Sell = () => {
-    const Shareblock = () => {
-        return (
-            <section className="block">
-                <button className="share">판매/나눔 게시판</button>
-                <Cwrite />
-            </section>
-        );
-
-    };
-
-    const Cwrite = () => {
-        return (
-            <section>
-                <button className="newstudy" style={{ marginLeft: "0px" }}>
-                    <Link to="/setboard_sell">글 작성하기</Link></button>
-            </section>
-        );
-    };
-
-    const [setNickname] = useState("");
-    const [setTitle] = useState("");
-    const [data, setData] = useState("");
+    const [data, setData] = useState([]);
+    const [id, setId] = useState([]);
     React.useEffect(() => {
         axios
-            .get("http://localhost:8080/studyPosts/category?category=sell")
+            .get("http://localhost:8080/studyPosts/category?category=%ED%8C%90%EB%A7%A4%2F%EB%82%98%EB%88%94%20%EA%B2%8C%EC%8B%9C%EA%B8%80")
             .then((response) => {
-                setData(response.data)
-                setNickname(response.data.nickname)
-                setTitle(response.data.title)
+                setId(response.data);
             });
-    }, []);
+
+        for (let i = 0; i < id.length; i++) {
+            axios.get(`http://localhost:8080/studyPost/info?id=${id[i]}`)
+                .then((response) => {
+                    if (data.some((element) => element.id === id[i]) === false) {
+                        const temp = data.concat(response.data);
+                        setData(temp);
+                    }
+                });
+        }
+    }, [id]);
+    data.sort((a, b) => {
+        return a.id - b.id;
+    });
+
     const columns = [
         {
             title: <div className="studyname">스터디 명</div>,
             dataIndex: "title",
-            key: "key",
-            render: (text, { key }) => <Link to={`/detailsell${key}`} state={{ key: key }}>{text}</Link>
+            key: "title",
+            render: (text, record, index) => (
+                <Link
+                    to={`/detailsell/${id[index]}`}
+                    state={{ boardId: id[index] }}
+                >
+                    {text}
+                </Link>
+            ),
         },
 
         {
