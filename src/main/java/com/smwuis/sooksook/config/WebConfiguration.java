82c3a51437a4d1a331b2f4972aa31d095ec5b8bf
GaGa-Kim <wn8925@sookmyung.ac.kr>
2022-08-12 11:21:02 +0900

    CorsMappings 추가

--- src/main/java/com/smwuis/sooksook/config/WebConfiguration.java
+++ src/main/java/com/smwuis/sooksook/config/WebConfiguration.java
@@ -1,7 +1,9 @@
 package com.smwuis.sooksook.config;
 
+import com.amazonaws.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +18,20 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(baseHandlerInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name()
+                );
     }
 
 }
